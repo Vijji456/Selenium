3624ad50b479b716f91df8d5815a1855796359ed
Vijji456 <52966557+Vijji456@users.noreply.github.com>
2019-07-16 20:53:26 +0530

    Added new project Selenium new batch

---
+++ Interfaces/HDFC1.java
@@ -0,0 +1,17 @@
+package Interfaces;
+
+public class HDFC1 implements RBI1 {
+
+	@Override
+	public void withdrawl1() {
+		System.out.println("I am the withdrawl method of HDFC1");
+	}
+
+	@Override
+	public void deposit1() {
+	System.out.println("I am the deposited method of HDFC1");
+		
+	}
+	
+
+}
