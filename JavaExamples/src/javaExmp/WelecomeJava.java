5df750e8b08c38d00515c884c36b37bcb32838c6
Vijji456 <52966557+Vijji456@users.noreply.github.com>
2019-07-21 11:04:43 +0530

    Added new class

---
+++ JavaExamples/src/javaExmp/WelecomeJava.java
@@ -0,0 +1,10 @@
+package javaExmp;
+
+public class WelecomeJava {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+		System.out.println("Welocme India");
+	}
+
+}
