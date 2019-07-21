5780a30a40436c1f8ddc2ae87a0c1d4a94a2b7c8
Vijji456 <52966557+Vijji456@users.noreply.github.com>
2019-07-21 09:00:39 +0530

    Added new class

---
+++ JavaExamples/src/javaExmp/ArrayValues1.java
@@ -0,0 +1,25 @@
+package javaExmp;
+
+import java.util.Scanner;
+
+public class ArrayValues1 {
+
+	public static void main(String[] args) {
+		int asize;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Entert the size of the array");
+		asize=sc.nextInt();
+		System.out.println("Enter the values into array");
+		int a[]=new int[asize];
+		int sum=0;
+		
+		for(int i=0;i<asize;i++)
+		{
+			a[i]=sc.nextInt();
+			sum=sum+a[i];
+		}
+		System.out.println(sum);
+
+	}
+
+}
