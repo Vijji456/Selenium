9aad424c7adb02b459929595ab4ca0ea70b7aa2f
Vijji456 <52966557+Vijji456@users.noreply.github.com>
2019-09-06 09:50:07 +0530

    MavenProject added

---
+++ SampleMavenProj/src/test/java/com/mavenproject/SampleMavenProj/BaseTestMaven.java
@@ -0,0 +1,10 @@
+package com.mavenproject.SampleMavenProj;
+
+import org.testng.annotations.Test;
+
+public class BaseTestMaven {
+  @Test
+  public void f() {
+	  System.out.println("Welcome to Maven");
+  }
+}
